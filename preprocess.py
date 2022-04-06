13777110c0952cd808c7712f9bf779f5d96c58b6
Abhi-alpha2 <shubhamgupta2421996@gmail.com>
2022-04-06 14:51:41 +0530

    fir regex

--- preprocess.py
+++ preprocess.py
@@ -20,7 +20,7 @@ def getstring(text):
 
 def preprocess(data):
 
-    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s[AaPp][Mm]-\s'
+    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s[AaPp][Mm]'
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
 
